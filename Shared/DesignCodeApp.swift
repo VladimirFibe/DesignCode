6f0f2a1270db92a209bd26b4a0934add5540c28e
Vladimir Fibe <motiw@me.com>
2021-03-03 11:33:44 +0600

    Initial Commit

---
+++ Shared/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  Shared
+//
+//  Created by Vladimir Fibe on 03.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

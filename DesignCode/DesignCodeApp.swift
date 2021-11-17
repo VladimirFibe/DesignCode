84d2faf8093d7c03faab15c1e8b907d099ee24b2
Vladimir Fibe <motiw@me.com>
2021-11-17 07:48:06 +0600

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Vladimir on 17.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

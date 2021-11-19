b6a65f85a6f40e4891052f72fa816b6f0a4b79ce
Vladimir Fibe <motiw@me.com>
2021-11-19 06:15:35 +0600

    5

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -16,6 +16,10 @@ struct ContentView: View {
           .resizable(resizingMode: .stretch)
           .aspectRatio(contentMode: .fit)
           .frame(width: 26.0, height: 26.0)
+          .cornerRadius(20)
+          .padding(9)
+          .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 20, style: .continuous))
+
         Text("SwiftUI for iOS 15")
           .font(.largeTitle)
         .fontWeight(.bold)
@@ -30,14 +34,17 @@ struct ContentView: View {
           .lineLimit(2)
           .foregroundColor(.secondary)
           .frame(maxWidth: .infinity, alignment: .leading)
-          .offset(y: 50)
       }
       .padding(.all, 20.0)
       .padding(.vertical, 20)
       .frame(height: 350.0)
-      .background(.ultraThinMaterial)
-//      .cornerRadius(30.0)
+      .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
       .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+      .overlay(
+        RoundedRectangle(cornerRadius: 30, style: .continuous)
+          .stroke(.linearGradient(colors: [.white.opacity(0.3), .black.opacity(0.1)], startPoint: .top, endPoint: .bottom))
+          .blendMode(.overlay)
+      )
       .padding(.horizontal, 20)
       .background(
         Image("Blob 1")
@@ -50,6 +57,7 @@ struct ContentView: View {
           .frame(height: 230)
           .offset(x: 32, y: -80)
       )
+
     }
       
   }
@@ -59,6 +67,6 @@ struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
       .preferredColorScheme(.light)
-      .previewDevice("iPhone 13 mini")
+      .previewDevice("iPhone 13 Pro Max")
   }
 }

9e6766d4d425589089fc246eb17d5f330a565dee
fibeapp <motiw@me.com>
2023-01-14 21:21:24 +0600

    DesignCodeiOS16

---
+++ DesignCodeiOS16/DesignCodeiOS16/DesignCodeiOS16App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS16App.swift
+//  DesignCodeiOS16
+//
+//  Created by Vladimir Fibe on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

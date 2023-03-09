c008d406a2b73c009917ef39a43e56b209f7af02
Vladimir <motiw@icloud.com>
2023-03-09 09:36:18 +0600

    Variable Color and Animation

--- DesignCodeiOS16/DesignCodeiOS16/ContentView.swift
+++ DesignCodeiOS16/DesignCodeiOS16/ContentView.swift
@@ -1,18 +1,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var time = 0.2
     var body: some View {
+        let roundRect = RoundedRectangle(cornerRadius: 10)
         VStack(alignment: .center, spacing: 20.0) {
-            Image(systemName: "timelapse", variableValue: 0.2)
+            Image(systemName: "timelapse", variableValue: time)
                 .imageScale(.large)
-                .font(.largeTitle)
+                .font(.system(size: 50))
+                .fontWeight(.thin)
                 .foregroundColor(.accentColor)
-            Text("Switching apps")
+            Text("Switching apps".uppercased())
+                .font(.largeTitle.width(.compressed))
                 .fontWeight(.bold)
-                .font(.largeTitle)
             
             Text("Tap and hold any part of the screen for 1 second to show the menu for switching between apps.")
                 .multilineTextAlignment(.center)
+                .foregroundColor(.secondary)
+                .fontWeight(.medium)
             
             Button(action: {
                 print("yes")
@@ -20,26 +25,31 @@ struct ContentView: View {
                 Text("Got it")
                     .padding(.vertical)
                     .frame(maxWidth: .infinity)
-                    .background(
-                        RoundedRectangle(cornerRadius: 10).stroke()
-                    )
+                    .background(roundRect.fill(.white.opacity(0.2).gradient))
+                    .background(roundRect.stroke(.linearGradient(colors: [.white.opacity(0.5), .clear, .white.opacity(0.5), .clear], startPoint: .topLeading, endPoint: .bottomTrailing)))
             }
             .accentColor(.primary)
+            .shadow(radius: 10)
         }
         .padding(30)
-        .background(RoundedRectangle(cornerRadius: 10).fill(.regularMaterial))
-        .overlay(RoundedRectangle(cornerRadius: 10).stroke())
+        .background(roundRect.fill(.regularMaterial))
+        .overlay(roundRect.stroke(.linearGradient(colors: [.white.opacity(0.5), .clear], startPoint: .top, endPoint: .bottom)))
+        .shadow(color: .black.opacity(0.3), radius: 20, y: 20)
+        .frame(maxWidth: 500)
         .padding(10)
+        .dynamicTypeSize(.xSmall ... .xxxLarge)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(
                 Image("Wallpaper 2")
                 .resizable()
                 .scaledToFill()
+                .ignoresSafeArea()
             )
     }
 }
--- DesignCodeiOS16/DesignCodeiOS16/DesignCodeiOS16App.swift
+++ DesignCodeiOS16/DesignCodeiOS16/DesignCodeiOS16App.swift
@@ -5,6 +5,13 @@ struct DesignCodeiOS16App: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(
+                    Image("Wallpaper 2")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+                )
         }
     }
 }

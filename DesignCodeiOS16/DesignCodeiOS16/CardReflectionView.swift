5db5a384c8e5dd09310d6c9a788fd663b9750dd8
Vladimir <motiw@icloud.com>
2023-03-23 14:07:34 +0600

    20

--- DesignCodeiOS16/DesignCodeiOS16/CardReflectionView.swift
+++ DesignCodeiOS16/DesignCodeiOS16/CardReflectionView.swift
@@ -26,10 +26,54 @@ struct CardReflectionView: View {
                             .blendMode(.overlay)
                     }
                 )
+                .overlay(gloss1.blendMode(.softLight))
+                .overlay(gloss2.blendMode(.luminosity))
+                .overlay(gloss2.blendMode(.overlay))
+                .overlay(LinearGradient(colors: [.clear, .white.opacity(0.5), .clear], startPoint: .topLeading, endPoint: .bottomTrailing))                .overlay(RoundedRectangle(cornerRadius: 50)
+                    .strokeBorder(.linearGradient(colors: [.clear, .white.opacity(0.75), .clear, .white.opacity(0.75), .clear], startPoint: .topLeading, endPoint: .bottomTrailing)))
+                .overlay(
+                    LinearGradient(
+                        colors: [Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 0.5)), Color(#colorLiteral(red: 0.2196078431, green: 0.007843137255, blue: 0.8549019608, alpha: 0.5))],
+                        startPoint: .topLeading,
+                        endPoint: .bottomTrailing)
+                        .blendMode(.overlay)
+                )
                 .cornerRadius(50)
+                .background(
+                RoundedRectangle(cornerRadius: 50)
+                    .fill(.black)
+                    .offset(y: 50)
+                    .blur(radius: 50)
+                    .opacity(0.5)
+                    .blendMode(.overlay)
+                )
                 .scaleEffect(0.9)
         }
         .preferredColorScheme(.dark)
+    }
+    var gloss1: some View {
+        Image("Gloss 1")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .mask(
+                LinearGradient(
+                    colors: [.clear, .white, .clear, .white, .clear, .white, .clear],
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing)
+                .frame(width: 392)
+            )
+    }
+    var gloss2: some View {
+        Image("Gloss 2")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .mask(
+                LinearGradient(
+                    colors: [.clear, .white, .clear, .white, .clear, .white, .clear],
+                    startPoint: .topLeading,
+                    endPoint: .bottomTrailing)
+                .frame(width: 392)
+            )
     }
 }
 
